ab6b26f9e625d6246de34097513fc661912d2ea2
AhmadAbdulhai\AHMAD <ahmad52585258@gmail.com>
2024-09-22 13:07:14 +0300

    Editi timers configuration(TIM1-->Green, TIM2-->Red, TIM3-->Blue)

--- P01R0/P01R0_timers.c
+++ P01R0/P01R0_timers.c
@@ -24,14 +24,16 @@ GPIO_InitTypeDef GPIO_InitStruct = {0};
 TIM_HandleTypeDef htim16; /* micro-second delay counter */
 TIM_HandleTypeDef htim17; /* milli-second delay counter */
 
+extern TIM_HandleTypeDef htim1;  /* Timer for RGB green */
 extern TIM_HandleTypeDef htim2;  /* Timer for RGB red */
 extern TIM_HandleTypeDef htim3;  /* Timer for RGB blue */
-extern TIM_HandleTypeDef htim4;  /* Timer for RGB green */
+
 IWDG_HandleTypeDef hiwdg;
 
+extern void MX_TIM1_Init(void);
 extern void MX_TIM2_Init(void);
 extern void MX_TIM3_Init(void);
-extern void MX_TIM4_Init(void);
+
 
 void HAL_TIM_MspPostInit(TIM_HandleTypeDef *htim);
 
@@ -55,6 +57,73 @@ void MX_IWDG_Init(void){
 	HAL_IWDG_Init(&hiwdg);
 
 }
+/* TIM1 init function */
+void MX_TIM1_Init(void)
+{
+
+	/* USER CODE BEGIN TIM1_Init 0 */
+	__TIM1_CLK_ENABLE();
+	/* USER CODE END TIM1_Init 0 */
+
+	TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+	TIM_MasterConfigTypeDef sMasterConfig = {0};
+	TIM_OC_InitTypeDef sConfigOC = {0};
+	TIM_BreakDeadTimeConfigTypeDef sBreakDeadTimeConfig = {0};
+
+	/* USER CODE BEGIN TIM1_Init 1 */
+
+	/* USER CODE END TIM1_Init 1 */
+	htim1.Instance = TIM1;
+	htim1.Init.Prescaler = (uint32_t )(HAL_RCC_GetSysClockFreq() / PWM_TIMER_CLOCK) - 1;
+	htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
+	htim1.Init.Period = 1023;
+	htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+	htim1.Init.RepetitionCounter = 0;
+	htim1.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+	HAL_TIM_Base_Init(&htim1);
+
+	sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+	HAL_TIM_ConfigClockSource(&htim1, &sClockSourceConfig);
+	HAL_TIM_PWM_Init(&htim1);
+
+	sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+	sMasterConfig.MasterOutputTrigger2 = TIM_TRGO2_RESET;
+	sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+	HAL_TIMEx_MasterConfigSynchronization(&htim1, &sMasterConfig);
+
+	sConfigOC.OCMode = TIM_OCMODE_PWM1;
+	sConfigOC.Pulse = 0;
+	sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+	sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+	sConfigOC.OCIdleState = TIM_OCIDLESTATE_RESET;
+	sConfigOC.OCNIdleState = TIM_OCNIDLESTATE_RESET;
+	HAL_TIM_PWM_ConfigChannel(&htim1, &sConfigOC, _RGB_GREEN_TIM_CH);
+
+	sBreakDeadTimeConfig.OffStateRunMode = TIM_OSSR_DISABLE;
+	sBreakDeadTimeConfig.OffStateIDLEMode = TIM_OSSI_DISABLE;
+	sBreakDeadTimeConfig.LockLevel = TIM_LOCKLEVEL_OFF;
+	sBreakDeadTimeConfig.DeadTime = 0;
+	sBreakDeadTimeConfig.BreakState = TIM_BREAK_DISABLE;
+	sBreakDeadTimeConfig.BreakPolarity = TIM_BREAKPOLARITY_HIGH;
+	sBreakDeadTimeConfig.BreakFilter = 0;
+	sBreakDeadTimeConfig.BreakAFMode = TIM_BREAK_AFMODE_INPUT;
+	sBreakDeadTimeConfig.Break2State = TIM_BREAK2_DISABLE;
+	sBreakDeadTimeConfig.Break2Polarity = TIM_BREAK2POLARITY_HIGH;
+	sBreakDeadTimeConfig.Break2Filter = 0;
+	sBreakDeadTimeConfig.Break2AFMode = TIM_BREAK_AFMODE_INPUT;
+	sBreakDeadTimeConfig.AutomaticOutput = TIM_AUTOMATICOUTPUT_DISABLE;
+	HAL_TIMEx_ConfigBreakDeadTime(&htim1, &sBreakDeadTimeConfig);
+
+	/* USER CODE BEGIN TIM1_Init 2 */
+
+	/* USER CODE END TIM1_Init 2 */
+	HAL_TIM_MspPostInit(&htim1);
+	/* Peripheral clock enable */
+	__HAL_RCC_TIM1_CLK_ENABLE();
+	/* TIM1 interrupt Init */
+	HAL_NVIC_SetPriority(TIM1_CC_IRQn, 0, 0);
+	HAL_NVIC_EnableIRQ(TIM1_CC_IRQn);
+}
 /* TIM2 init function */
 void MX_TIM2_Init(void)
 {
@@ -65,7 +134,7 @@ void MX_TIM2_Init(void)
 
 	TIM_MasterConfigTypeDef sMasterConfig = {0};
 	TIM_OC_InitTypeDef sConfigOC = {0};
-	TIM_ClockConfigTypeDef sClockSourceConfig;
+	TIM_ClockConfigTypeDef sClockSourceConfig = {0};
 
 	/* USER CODE BEGIN TIM2_Init 1 */
 
@@ -76,10 +145,11 @@ void MX_TIM2_Init(void)
 	htim2.Init.Period = 1023;
 	htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
 	htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
-	HAL_TIM_PWM_Init(&htim2);
+	HAL_TIM_Base_Init(&htim2);
 
 	sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
 	HAL_TIM_ConfigClockSource(&htim2,&sClockSourceConfig);
+	HAL_TIM_PWM_Init(&htim2);
 
 	sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
 	sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
@@ -132,6 +202,7 @@ void MX_TIM3_Init(void)
 
 	sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
 	HAL_TIM_ConfigClockSource(&htim3, &sClockSourceConfig);
+	HAL_TIM_PWM_Init(&htim3);
 
 	sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
 	sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
@@ -149,133 +220,82 @@ void MX_TIM3_Init(void)
 	HAL_TIM_MspPostInit(&htim3);
 
 	/* TIM3 clock enable */
-	 __HAL_RCC_TIM3_CLK_ENABLE();
+	__HAL_RCC_TIM3_CLK_ENABLE();
 
-	 /* TIM3 interrupt Init */
-	 HAL_NVIC_SetPriority(TIM3_TIM4_IRQn, 0, 0);
-	 HAL_NVIC_EnableIRQ(TIM3_TIM4_IRQn);
-
-}
-/*-----------------------------------------------------------*/
-/* TIM4 init function */
-void MX_TIM4_Init(void)
-{
-	/* USER CODE BEGIN TIM4_Init 0 */
-	__TIM4_CLK_ENABLE();
-	/* USER CODE END TIM4_Init 0 */
-
-	TIM_ClockConfigTypeDef sClockSourceConfig = {0};
-	TIM_MasterConfigTypeDef sMasterConfig = {0};
-	TIM_OC_InitTypeDef sConfigOC = {0};
-
-	/* USER CODE BEGIN TIM4_Init 1 */
-
-	/* USER CODE END TIM4_Init 1 */
-	htim4.Instance = TIM4;
-	htim4.Init.Prescaler = (uint32_t )(HAL_RCC_GetSysClockFreq() / PWM_TIMER_CLOCK) - 1;
-	htim4.Init.CounterMode = TIM_COUNTERMODE_UP;
-	htim4.Init.Period = 1023;
-	htim4.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
-	htim4.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
-	HAL_TIM_Base_Init(&htim4);
-
-	sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
-	HAL_TIM_ConfigClockSource(&htim4, &sClockSourceConfig);
-
-	sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
-	sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
-	HAL_TIMEx_MasterConfigSynchronization(&htim4, &sMasterConfig);
-
-	sConfigOC.OCMode = TIM_OCMODE_PWM1;
-	sConfigOC.Pulse = 0;
-	sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
-	sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
-	HAL_TIM_PWM_ConfigChannel(&htim4, &sConfigOC, _RGB_GREEN_TIM_CH);
-
-	/* USER CODE BEGIN TIM4_Init 2 */
-
-	/* USER CODE END TIM4_Init 2 */
-	HAL_TIM_MspPostInit(&htim4);
-
-	/* TIM4 clock enable */
-	  __HAL_RCC_TIM4_CLK_ENABLE();
-
-	  /* TIM4 interrupt Init */
-	  HAL_NVIC_SetPriority(TIM3_TIM4_IRQn, 0, 0);
-	  HAL_NVIC_EnableIRQ(TIM3_TIM4_IRQn);
+	/* TIM3 interrupt Init */
+	HAL_NVIC_SetPriority(TIM3_TIM4_IRQn, 0, 0);
+	HAL_NVIC_EnableIRQ(TIM3_TIM4_IRQn);
 
 }
+
 /*-----------------------------------------------------------*/
 
 void HAL_TIM_MspPostInit(TIM_HandleTypeDef* timHandle)
 {
 
-GPIO_InitTypeDef GPIO_InitStruct = {0};
-if(timHandle->Instance==TIM2)
-{
-	/* USER CODE BEGIN TIM2_MspPostInit 0 */
+	GPIO_InitTypeDef GPIO_InitStruct = {0};
+	if(timHandle->Instance==TIM1)
+	{
+		/* USER CODE BEGIN TIM1_MspPostInit 0 */
 
-	/* USER CODE END TIM2_MspPostInit 0 */
-	__HAL_RCC_GPIOA_CLK_ENABLE();
-	/**TIM2 GPIO Configuration
+		/* USER CODE END TIM1_MspPostInit 0 */
+		__HAL_RCC_GPIOA_CLK_ENABLE();
+		/**TIM1 GPIO Configuration
+        PA11 [PA9] ------> TIM1_CH4
+		 */
+		GPIO_InitStruct.Pin = _RGB_GREEN_PIN;
+		GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+		GPIO_InitStruct.Pull = GPIO_NOPULL;
+		GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+		GPIO_InitStruct.Alternate = GPIO_AF2_TIM1;
+		HAL_GPIO_Init(_RGB_GREEN_PORT, &GPIO_InitStruct);
+
+		/* USER CODE BEGIN TIM1_MspPostInit 1 */
+
+		/* USER CODE END TIM1_MspPostInit 1 */
+	}
+	else if(timHandle->Instance==TIM2)
+	{
+		/* USER CODE BEGIN TIM2_MspPostInit 0 */
+
+		/* USER CODE END TIM2_MspPostInit 0 */
+		__HAL_RCC_GPIOA_CLK_ENABLE();
+		/**TIM2 GPIO Configuration
 	PA15     ------> TIM2_CH1
-	*/
-	GPIO_InitStruct.Pin = _RGB_RED_PIN;
-	GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-	GPIO_InitStruct.Pull = GPIO_NOPULL;
-	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-	GPIO_InitStruct.Alternate = GPIO_AF2_TIM2;
-	HAL_GPIO_Init(_RGB_RED_PORT, &GPIO_InitStruct);
+		 */
+		GPIO_InitStruct.Pin = _RGB_RED_PIN;
+		GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+		GPIO_InitStruct.Pull = GPIO_NOPULL;
+		GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+		GPIO_InitStruct.Alternate = GPIO_AF2_TIM2;
+		HAL_GPIO_Init(_RGB_RED_PORT, &GPIO_InitStruct);
 
-	/* USER CODE BEGIN TIM2_MspPostInit 1 */
+		/* USER CODE BEGIN TIM2_MspPostInit 1 */
 
-	/* USER CODE END TIM2_MspPostInit 1 */
-}
+		/* USER CODE END TIM2_MspPostInit 1 */
+	}
 
-else if(timHandle->Instance==TIM3)
-{
-	/* USER CODE BEGIN TIM3_MspPostInit 0 */
+	else if(timHandle->Instance==TIM3)
+	{
+		/* USER CODE BEGIN TIM3_MspPostInit 0 */
 
-	/* USER CODE END TIM3_MspPostInit 0 */
+		/* USER CODE END TIM3_MspPostInit 0 */
 
-	__HAL_RCC_GPIOA_CLK_ENABLE();
-	/**TIM3 GPIO Configuration
+		__HAL_RCC_GPIOA_CLK_ENABLE();
+		/**TIM3 GPIO Configuration
 	PA6     ------> TIM3_CH1
-	*/
-	GPIO_InitStruct.Pin = _RGB_BLUE_PIN;
-	GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-	GPIO_InitStruct.Pull = GPIO_NOPULL;
-	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-	GPIO_InitStruct.Alternate = GPIO_AF1_TIM3;
-	HAL_GPIO_Init(_RGB_BLUE_PORT, &GPIO_InitStruct);
+		 */
+		GPIO_InitStruct.Pin = _RGB_BLUE_PIN;
+		GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+		GPIO_InitStruct.Pull = GPIO_NOPULL;
+		GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+		GPIO_InitStruct.Alternate = GPIO_AF1_TIM3;
+		HAL_GPIO_Init(_RGB_BLUE_PORT, &GPIO_InitStruct);
 
-	/* USER CODE BEGIN TIM3_MspPostInit 1 */
+		/* USER CODE BEGIN TIM3_MspPostInit 1 */
 
-	/* USER CODE END TIM3_MspPostInit 1 */
-}
-
-else if(timHandle->Instance==TIM4)
-{
-	/* USER CODE BEGIN TIM4_MspPostInit 0 */
-
-	/* USER CODE END TIM4_MspPostInit 0 */
-
-	__HAL_RCC_GPIOB_CLK_ENABLE();
-	/**TIM4 GPIO Configuration
-	PB7     ------> TIM4_CH2
-	*/
-	GPIO_InitStruct.Pin = _RGB_GREEN_PIN;
-	GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-	GPIO_InitStruct.Pull = GPIO_NOPULL;
-	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-	GPIO_InitStruct.Alternate = GPIO_AF9_TIM4;
-	HAL_GPIO_Init(_RGB_GREEN_PORT, &GPIO_InitStruct);
-
-	/* USER CODE BEGIN TIM4_MspPostInit 1 */
-
-	/* USER CODE END TIM4_MspPostInit 1 */
-}
-
+		/* USER CODE END TIM3_MspPostInit 1 */
+	}
 }
 
 /*-----------------------------------------------------------*/
@@ -311,7 +331,7 @@ void TIM_MSEC_Init(void){
 	htim17.Init.RepetitionCounter = 0;
 	htim17.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
 	HAL_TIM_Base_Init(&htim17);
-	
+
 	HAL_TIM_Base_Start(&htim17);
 }
 
@@ -321,12 +341,12 @@ void TIM_MSEC_Init(void){
  */
 void StartMicroDelay(uint16_t Delay){
 	uint32_t t0 =0;
-	
+
 	portENTER_CRITICAL();
-	
+
 	if(Delay){
 		t0 =htim16.Instance->CNT;
-		
+
 		while(htim16.Instance->CNT - t0 <= Delay){};
 	}
 
@@ -339,12 +359,12 @@ void StartMicroDelay(uint16_t Delay){
  */
 void StartMilliDelay(uint16_t Delay){
 	uint32_t t0 =0;
-	
+
 	portENTER_CRITICAL();
-	
+
 	if(Delay){
 		t0 =htim17.Instance->CNT;
-		
+
 		while(htim17.Instance->CNT - t0 <= Delay){};
 	}
 
